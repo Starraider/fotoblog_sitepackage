12411042dd5ac26adf99c92014a549f9778ca651
Sven K <sven@skom.de>
2021-06-01 13:07:53 +0530

    fix: Fix ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,23 @@
 <?php
-
-/**
- * Extension Manager/Repository config file for ext "fotoblog_sitepackage".
- */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Fotoblog Sitepackage',
     'description' => 'Sitepackage for a Foto- or Travel-Blog',
     'category' => 'templates',
+    'author' => 'Sven Kalbhenn',
+    'author_email' => 'sven@skom.de',
+    'author_company' => 'SKom',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.25-10.4.99',
+            'typo3' => '9.5.25-11.99.99',
             'bootstrap_package' => '11.0.0-11.99.99',
-            'skom_sitepackage' => '9.2.0-9.99.99'
+            'skom_sitepackage' => '9.2.0-9.99.99',
         ],
         'conflicts' => [
+        ],
+        'suggests' => [
         ],
     ],
     'autoload' => [
@@ -21,12 +25,4 @@ $EM_CONF[$_EXTKEY] = [
             'Skom\\FotoblogSitepackage\\' => 'Classes'
         ],
     ],
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
-    'author' => 'Sven Kalbhenn',
-    'author_email' => 'sven@skom.de',
-    'author_company' => 'SKom',
-    'version' => '1.2.0',
 ];
